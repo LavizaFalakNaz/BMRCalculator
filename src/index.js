12d1bbce4b7131b39580991b5cc008a0a3316e2b
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2021-03-11 23:06:47 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,3 +4,8 @@ import ReactDOM from "react-dom";
 import App from "./App";
 const rootElement = document.getElementById("root");
 ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
