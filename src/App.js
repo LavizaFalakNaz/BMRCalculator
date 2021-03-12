f48f8a50a6012797bd51347a86b23239abd63615
Laviza Falak Naz <60230431+LavizaFalakNaz@users.noreply.github.com>
2021-03-12 21:37:24 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import Bmr from './components/Bmr';
+
+export default function App() {
+  return (
+    <div className="App">
+      <Bmr/>
+    </div>
+  );
+}
